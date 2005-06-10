5542ba3b920de1937ffcab0a17a876beef41387d
nathansamson <nathansamson@f7b65379-26f4-0310-9526-bc14f37e3617>
2005-06-10 18:00:33 +0000

    file was not needed

--- site.config.php
+++
@@ -1,7 +0,0 @@
-<?php $config['general']['servertimezone'] = +2;$config['general']['timezone'] = 0;
-$config['general']['httplink'] = 'http://yapcas.localhost';
-$config['general']['webmastermail'] = 'nathansamson@gmail.com';
-$config['general']['timeformat'] = 'H:i:s d/m/y';$config['general']['sitename'] = 'YaPCaS';
-$config['general']['description'] = 'Yet another PHP Content admin System';
-$config['general']['language'] = 'dutch';$config['general']['theme'] = 'moderngray';
-$config['general']['databasetype'] = postgresql;$config['general']['errorreporting'] = E_NONE;$config['database']['host'] = 'localhost';$config['database']['user'] = 'nathan';$config['database']['password'] = 'TuxPHP';$config['database']['name'] = 'yapcas';$config['news']['headlines'] = 10;$config['news']['postsonpage'] = 5;$config['news']['threaded'] = true;$config['user']['activatemail'] = true; ?>
